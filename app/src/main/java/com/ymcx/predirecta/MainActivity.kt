8763568f71f3594e282030f7df9486907a6352ad
ymcx <89810988+ymcx@users.noreply.github.com>
2024-04-28 17:23:28 +0300

    Make changing the redirection url possible

--- app/src/main/java/com/ymcx/predirecta/MainActivity.kt
+++ app/src/main/java/com/ymcx/predirecta/MainActivity.kt
@@ -4,12 +4,37 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.app.AlertDialog
+import android.content.SharedPreferences
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    lateinit var sharedPref: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(null)
-        val data = intent.data.toString()
-        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://yewtu.be" + data.substring(data.lastIndexOf('/'), data.length))))
-        finish()
+        super.onCreate(savedInstanceState)
+        sharedPref = getPreferences(MODE_PRIVATE)
+        if (intent?.action == Intent.ACTION_VIEW) {
+            val oldUrl = intent.data.toString()
+            val address = sharedPref.getString(getString(R.string.url), null)
+            val parameters = oldUrl.substring(oldUrl.lastIndexOf('/'), oldUrl.length)
+            val url = Uri.parse("https://" + address + parameters)
+            startActivity(Intent(Intent.ACTION_VIEW, url))
+            finish()
+        }
+        else {
+            val builder = AlertDialog.Builder(this)
+            builder.apply {
+                val input = EditText(context)
+                builder.setView(input)
+                setPositiveButton("Save") { _, _ ->
+                    with(sharedPref.edit()) {
+                        putString(getString(R.string.url), input.text.toString())
+                        apply()
+                    }
+                    finish()
+                }
+            }
+            builder.show()
+        }
     }
 }
