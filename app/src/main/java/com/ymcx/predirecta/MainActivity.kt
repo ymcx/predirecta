63e0ca6f4aa03ff1419385566cb4545ee2daa473
ymcx <89810988+ymcx@users.noreply.github.com>
2024-07-31 19:45:53 +0300

    potential fix for a bug where redirection doesn't work after force close

--- app/src/main/java/com/ymcx/predirecta/MainActivity.kt
+++ app/src/main/java/com/ymcx/predirecta/MainActivity.kt
@@ -7,30 +7,32 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.browser.customtabs.CustomTabsIntent
 
 class MainActivity : AppCompatActivity() {
+    fun openUrl(url: String) {
+        CustomTabsIntent.Builder().build().launchUrl(this, Uri.parse(url))
+    }
     override fun onCreate(bundle: Bundle?) {
         super.onCreate(bundle)
         val prefs = getPreferences(MODE_PRIVATE)
         val prefsInstance = getString(R.string.instance)
-        var url = prefs.getString(prefsInstance, "redirect.invidious.io")
+        val instance = prefs.getString(prefsInstance, "redirect.invidious.io").orEmpty()
         when (intent.action) {
             Intent.ACTION_SEND -> {
-                url = intent.getStringExtra(Intent.EXTRA_TEXT).orEmpty()
-                    .replace("\"", "")
-                    .replace("http://", "")
-                    .replace("https://", "")
-                    .split("/")[0]
+                val url = intent.getStringExtra(Intent.EXTRA_TEXT).orEmpty()
+                    .replace(Regex("(\"|https://|http://|/.*)"), "")
                 with (prefs.edit()) {
                     putString(prefsInstance, url)
                     apply()
                 }
             }
             Intent.ACTION_VIEW -> {
-                url = intent.getData().toString()
-                    .replace(Regex("(.*)youtube.com/"), url+"/")
-                    .replace(Regex("(.*)youtu.be/"),    url+"/watch?v=")
+                val url = intent.getData().toString()
+                    .replace(Regex("(m.youtube.com|www.youtube.com|youtube.com|youtu.be)"), instance)
+                openUrl(url)
+            }
+            Intent.ACTION_MAIN -> {
+                openUrl("https://"+instance)
             }
         }
-        CustomTabsIntent.Builder().build().launchUrl(this, Uri.parse("https://"+url))
         finish()
     }
 }
